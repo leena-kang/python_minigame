7c29a0c9b351ce87d1d2ec6c7a8b674f2f13c190
Leena Kang <leenak100@gmail.com>
2022-07-29 14:08:31 -0700

    madlibs

---
+++ 1_madlibs/madlibs2.py
@@ -0,0 +1,27 @@
+noun1 = input("Noun: ") 
+verb1 = input("Verb: ")
+type_of_liquid = input("Type of liquid: ") 
+animal = input("Animal: ")
+type_of_food = input("Type of food: ")
+plural_noun1 = input("Plural Noun: ")
+number = input("Number: ")
+plural_noun2 = input("Plural Noun: ")
+plural_noun3 = input("Plural Noun: ")
+adjective1 = input("Adjective: ")
+plural_noun4 = input("Plural Noun: ")
+adjective2 = input("Adjective: ")
+type_of_food2 = input("Type of Food: ")
+plural_noun5 = input("Plural Noun: ")
+a_place = input("A place: ")
+
+
+madlib =  f"DAY IN THE LIFE: TEACHER EDITION\n\n 6:00 am.: Wake up to the sound of the {noun1} beeping. {verb1} out of bed and go immedeiately to the \
+{type_of_liquid}-maker. Ahhh... much better! \n 7:30 am.: Walk the {animal}, eat {type_of_food} for breakfast, \
+and drive to school while listening to {plural_noun1}, band back pop {plural_noun2}, answer questions sabout how to get extra credit.\
+\n 12:00 pm.: Only {number} minutes for lunch! Eat leftover {type_of_food} as quickly as possible, And \
+drink annother cup of {type_of_liquid}, of course! \n 3:00 pm.: School day is over for the {plural_noun3}, but \
+the {adjective1} work has only begun. Grade {plural_noun4}, write assignments, hand {adjective2} decorations in classroom. \
+\n 5:00 pm.: Drive home, heat up {type_of_food2} for dinner in the microwave, fall asleep while wathing \
+The Real {plural_noun5} of (the) {a_place} on television! "
+
+print(madlib)
